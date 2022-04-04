2d192fc98ac4760e55e7d3c029ebe7508e8adde2
nowayman745 <103005201+nowayman745@users.noreply.github.com>
2022-04-04 18:54:26 -0400

    Add files via upload
    First python file

---
+++ new.py
@@ -0,0 +1,19 @@
+print(" Movies and it's latest news")
+name= input ("  What is your name? ")
+print( "Hello,"+name)
+questions= input( "Who is your favorite marvel character?  ")
+print("Mine is,"+ questions)
+
+print(" To know all the latest news about movies,please answer the following questions")
+print(" This is just to filter out the best results for you")
+
+questionss= input('What do you like better, DC or Marvel? ')
+print( "I like ," + questionss)
+questionsss= input(" Who is your favorite actor?")
+print("Mine is,"+ questionsss)
+reigon= input(" do you watch any foreign movies? For example, korean, indian, etc.?, Please answer as do, or do not")
+print("I " + reigon)
+
+
+
+
